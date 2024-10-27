1cd6e179aaa7d35816ecd60cb54855e9bfd634f7
msm19682003 <msm19682003@gmail.com>
2024-10-28 00:25:12 +0600

    day2

---
+++ Week 1/Day 2/Biscuit Generator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int a,b,t;
+    cin>>a>>b>>t;
+    int s=t/a;
+    int res= s*b;
+    cout<<res<<endl;
+}
