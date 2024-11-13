aff459395ae958f40f31c3e6b335a841f99c9d4e
msm19682003 <msm19682003@gmail.com>
2024-11-13 18:29:46 +0600

    W3 D3 modified

--- Week 3/Day 3/Line.cpp
+++ Week 3/Day 3/Line.cpp
@@ -1,48 +1,39 @@
-#include<bits/stdc++.h>
-#define ll long long
-
+#include <bits/stdc++.h>
+#define ll long long int
+#define sp ' '
 using namespace std;
 
 void solve()
 {
-    ll n,i,j;
-    cin>>n;
-    ll a[n][n-1];
-    for(i=0;i<n;i++)
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> res(n);
+    ll total = 0;
+    for (int i = 0; i < n; i++)
     {
-        for(j=0;j<n-1;j++)
-        {
-            cin>>a[i][j];
-        }
-    }
-    ll res[n+1]={0};
-    vector<int> v;
-    for(j=0;j<n-1;j++)
-    {
-        for(i=0;i<n;i++)
-        {
-            res[a[i][j]]++;
-            if(res[a[i][j]]==n-1)
-            {
-                v.push_back(a[i][j]);
-            }
-        }
+        int x;
+        if(s[i] == 'L') x = i;
+        else if(s[i] == 'R') x = (n-1) - i;
+        res[i] = max(i, (n-1)-i) - x;
+        total+= x;
     }
 
-    for(auto ele:v)
+    sort(res.begin(), res.end(), greater<int>());
+    for (int x : res)
     {
-        cout<<ele<<" ";
+        total+=x;
+        cout <<  total << sp;
     }
-    cout<<endl;
+    cout << endl;
 }
-
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t;
-    cin>>t;
-    while(t--)
-    {
-        solve();
-    }
+    cin >> t; while(t--)
+    solve();
+    return 0;
 }
-
