4da42f7d44f8677f06961f127ed3d8757b3cc169
msm19682003 <msm19682003@gmail.com>
2025-01-10 23:50:15 +0600

    w11 d6

---
+++ Week 11/Day 6/w_11_t5_E_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fast ios::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long int
+#define sp ' ' 
+#define py cout << "YES" << '\n';
+#define pn cout << "NO" << '\n';
+#define nl cout << '\n'
+using namespace std;
+
+
+void solve() {
+    /* code */
+}
+
+int main() {
+    fast;
+    int t;
+    cin >> t; while(t--)
+    solve();
+    return 0;
+}
